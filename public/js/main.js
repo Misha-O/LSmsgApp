e8a1cbd134468e7b104b4370336faf19a46a167d
Misha Oleinychenko <m.oleinychenko@gmail.com>
2020-12-21 17:22:43 -0500

    refactoring

--- public/js/main.js
+++ public/js/main.js
@@ -1,82 +1,143 @@
-const chatForm = document.getElementById("chat-form");
-const chatMessages = document.querySelector(".chat-messages");
-const userList = document.getElementById("users");
-const userAvatar = document.querySelector(".user--avatar");
-const currentUser = document.querySelector(".current--user");
+const notification = document.querySelector(".notification");
+const logWindow = document.querySelector(".login");
+const logInput = document.querySelector(".login__input");
+const logBtn = document.querySelector(".login__btn");
+const chatContainer = document.querySelector(".chat");
+const currentContainerName = document.querySelector(".current__user");
+const usersContainer = document.querySelector(".users");
+const sendBtn = document.querySelector(".send__btn");
+const msgInput = document.querySelector(".send__input");
+const messagesContainer = document.querySelector(".messages");
+const userAvatar = document.querySelector(".user_avatar");
 
-// get username from URL
-// Qs library to query string from http address, to ignore spec characters add ignoreQueryPrefix
-const { username, chatRoom } = Qs.parse(location.search, {
-  ignoreQueryPrefix: true,
+let socket = null; //to use it inside functions
+let userName = "";
+
+logBtn.addEventListener("click", (e) => {
+  socket = new WebSocket("ws://localhost:3000");
+  // listen for events received from server, which before we received by clicking 'enter', opened ws and send to the server
+  socket.addEventListener("open", () => {
+    logWindow.classList.add("hidden");
+    chatContainer.classList.remove("hidden");
+    userName = logInput.value;
+    currentContainerName.textContent = userName;
+    userAvatar.setAttribute("data-avatar", userName);
+    // create object with username to send to server
+    const request = {
+      type: "login",
+      name: userName,
+    };
+    // ws can operate strings only, thus
+    socket.send(JSON.stringify(request));
+  });
+
+  // set up event
+  socket.addEventListener("message", (e) => {
+    // storing object with data
+    const wsObjReceived = JSON.parse(e.data);
+
+    // from prev 'request obj' determine type(its value)
+    switch (wsObjReceived.type) {
+      case "login":
+        if (wsObjReceived.fromUser !== userName) {
+          showNotification(`User ${wsObjReceived.fromUser} entered the chat`);
+        }
+        outputChatUsers(wsObjReceived.data);
+        break;
+      case "log-out":
+        showNotification(`User ${wsObjReceived.fromUser} has left the chat`);
+        outputChatUsers(wsObjReceived.data);
+        break;
+      case "message":
+        outputMessage(wsObjReceived.data);
+        break;
+      case "avatar":
+        updateAvatar(wsObjReceived.data);
+        if (wsObjReceived.fromUser !== userName) {
+          showNotification(
+            `User ${wsObjReceived.fromUser} has changed the profile picture`
+          );
+        }
+      default:
+        break;
+    }
+  });
 });
 
-const socket = io();
+sendBtn.addEventListener("click", () => {
+  const message = msgInput.value;
 
-// join chatroom
-socket.emit("joinChat", { username, chatRoom });
-
-// get chat users
-socket.on("chatUsers", ({ users }) => {
-  outputChatUsers(users);
+  const request = {
+    type: "message",
+    data: message,
+  };
+  socket.send(JSON.stringify(request));
+  msgInput.value = "";
+  msgInput.focus();
 });
-
-// listen for events received from server, which before we received from user actions and send to the server
-socket.on("message", (message) => {
-  outputMessage(message);
-
-  //   scroll down on msg
-  chatMessages.scrollTop = chatMessages.scrollHeight; //set to chatMsg height
-});
-
-// message submit
-chatForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  //   get msg text
-  const msg = e.target.elements.msg.value;
-
-  //   emitting msg to the server
-  socket.emit("chatMessage", msg);
-  //   clear msg inputs
-  e.target.elements.msg.value = "";
-  e.target.elements.msg.focus();
-});
-
-// output message to DOM
-//bz msg already object, format inside to access value
-function outputMessage(message) {
-  const div = document.createElement("div");
-  div.classList.add("message");
-  div.innerHTML = `<p class="meta">${message.username} <span>${message.time} </span></p>
-  <p class="text">${message.text}</p>`;
-  document.querySelector(".chat-messages").appendChild(div);
-}
 
 function outputChatUsers(users) {
-  userList.innerHTML = `
+  usersContainer.innerHTML = `
   ${users
     .map(
       (user) => `
-      <li class="user--item">
-      <div class="avatar" data-avatar="${user.username}">
-      <img src="${user.image ? user.image : "/img/no-photo.png"}" alt="" />
-      </div>
-      <div class="user--name">${user.username}</div>
+      <li class="user__item">
+        <div class="avatar" data-avatar="${user.userLogin}">
+          <img src="${
+            user.image ? user.image : "/img/no-photo.png"
+          }" alt="User profile photo" />
+        </div>
+        <div class="user__name">${user.userLogin}</div>
       </li>`
     )
     .join("")}
   `;
 }
 
-socket.on("imgLoad", ({ data }) => {
-  updateAvatar(data.data);
+function outputMessage(wsObjReceived) {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, 0); //pads string(with 0) to given length (2)
+  const minutes = String(date.getMinutes()).padStart(2, 0);
+  const time = `${hours}:${minutes}`;
+
+  const div = document.createElement("div");
+  div.classList.add("message");
+
+  div.innerHTML = `
+  <div class="avatar" data-avatar="${wsObjReceived.fromUser}">
+    <img src="${
+      wsObjReceived.image ? wsObjReceived.image : "/img/no-photo.png"
+    }" alt="" />
+  </div>
+  <div class="message-data">
+    <div class="user__name">${
+      wsObjReceived.fromUser
+    }  <span class="message__time">${time}</span></div>
+    <div class="text">${wsObjReceived.message}</div>
+  </div>`;
+
+  messagesContainer.append(div);
+}
+
+function showNotification(message) {
+  notification.textContent = message;
+  notification.classList.add("active");
+
+  setTimeout(() => {
+    notification.classList.remove("active");
+  }, 3000);
+}
+
+userAvatar.addEventListener("dragover", (e) => {
+  if (e.dataTransfer.items.length && e.dataTransfer.items[0].kind === "file") {
+    e.preventDefault();
+  }
 });
 
 userAvatar.addEventListener("drop", (e) => {
   const file = e.dataTransfer.items[0].getAsFile();
-  // construct a set of key/value pairs representing form fields and their values
+
   const formData = new FormData();
-  // appends value file to a key image
   formData.append("image", file);
 
   fetch("/upload", {
@@ -84,20 +145,23 @@ userAvatar.addEventListener("drop", (e) => {
     body: formData,
   })
     .then((res) => res.json())
-    .then((image) => {
+    .then(({ image }) => {
       const request = {
         type: "avatar",
         data: image,
       };
-      console.log(request);
       socket.send(JSON.stringify(request));
     });
+
   e.preventDefault();
 });
 
-userAvatar.addEventListener("dragover", (e) => {
-  // if any elem exist and it is a file(taking first element [0])
-  if (e.dataTransfer.items.length && e.dataTransfer.items[0].kind === "file") {
-    e.preventDefault();
+function updateAvatar(wsObjReceived) {
+  const avatarsDom = document.querySelectorAll(
+    `.avatar[data-avatar="${wsObjReceived.fromUser}"] img`
+  );
+
+  for (const img of avatarsDom) {
+    img.src = wsObjReceived.image;
   }
-});
+}
--- server.js
+++ server.js
@@ -1,15 +1,7 @@
-const path = require("path"); // node core module
 const http = require("http");
+const path = require("path"); //node core module
 const express = require("express");
-const socket = require("socket.io");
-const multer = require("multer"); //loads files on server side
-const formatMessage = require("./utils/messages");
-const {
-  userJoin,
-  getCurrentUser,
-  userLeave,
-  getChatRoomUsers,
-} = require("./utils/users");
+const multer = require("multer"); // load files on server side
 
 // img upload setup
 // all files stored on server as file
@@ -25,81 +17,136 @@ const storage = multer.diskStorage({
 // call multer
 const upload = multer({ storage });
 
+const WebSocket = require("ws");
+
 // app setup
 const app = express();
-const server = http.createServer(app); //access directly to handle socket.io
 
 // set static folder which serves files to server
 app.use(express.static(path.join(__dirname, "public")));
-const botName = "Service Bot";
-
-// socket setup(to operate in one port)
-const io = socket(server);
-
-// run when client connects
-io.on("connection", (socket) => {
-  socket.on("joinChat", ({ username, chatRoom }) => {
-    const user = userJoin(socket.id, username, chatRoom);
-
-    socket.join(user.chatRoom);
-
-    //   emits only to current user
-    socket.emit("message", formatMessage(botName, "Welcome to LS Chat"));
-
-    //   broadcast when client connects
-    socket.broadcast
-      .to(user.chatRoom)
-      .emit(
-        "message",
-        formatMessage(botName, `${user.username} has joined the chat`)
-      );
-
-    // send chat users info to sidebar
-    io.to(user.chatRoom).emit("chatUsers", {
-      chatRoom: user.chatRoom,
-      users: getChatRoomUsers(user.chatRoom),
-    });
-
-    // socket.broadcast.to(user.chatRoom).emit("imgLoad", (data) => {
-    //   const image = data.data;
-    //   io.to(user.chatRoom).emit("imgLoad");
-    // });
-  });
-
-  //   listen for chatMessage
-  socket.on("chatMessage", (msg) => {
-    const user = getCurrentUser(socket.id);
-    // emit back to client
-    io.to(user.chatRoom).emit("message", formatMessage(user.username, msg));
-  });
-
-  //   when client disconnects
-  socket.on("disconnect", () => {
-    const user = userLeave(socket.id);
-
-    // if exists
-    if (user) {
-      io.to(user.chatRoom).emit(
-        "message",
-        formatMessage(botName, `${user.username} has left the chat`)
-      );
-
-      io.to(user.chatRoom).emit("chatUsers", {
-        chatRoom: user.chatRoom,
-        users: getChatRoomUsers(user.chatRoom),
-      });
-    }
-  });
-});
-
-// if no 3000 will look for environment vrbl named port
-const PORT = 3000 || process.env.PORT;
-server.listen(PORT, () => {
-  console.log(`connection established on port: ${PORT}`);
-});
 
 // upload images as avatars
 // route which on POST with url 'upload'. Then multer will load img -> in req with property file we receive all info
 app.post("/upload", upload.single("image"), (req, res) => {
   res.status(200).send({ image: `/photos/${req.file.filename}` });
+});
+
+// start server
+const server = http.createServer(app);
+
+// socket setup(to operate in one port)
+const webSocketServer = new WebSocket.Server({ server });
+
+// to hold keys and values received from ws(after parsing)
+const connections = new Map();
+
+// run when client connects
+webSocketServer.on("connection", (socket) => {
+  // storing data (key, value)
+  connections.set(socket, {});
+
+  socket.on("message", (message) => {
+    // to get object with data instead of simple string and define further actions from server
+    const wsObjReceived = JSON.parse(message);
+    // console.log(wsObjReceived);
+
+    switch (wsObjReceived.type) {
+      case "login":
+        connections.get(socket).userLogin = wsObjReceived.name;
+        connections.get(socket).image = "";
+
+        // setting up initial login and Boolean false, no broadcast, but if other user joins Boolean === true and socket opens
+        broadcastMessage(
+          {
+            type: "login",
+            data: [...connections.values()]
+              .map((item) => ({
+                userLogin: item.userLogin,
+                image: item.image,
+              }))
+              .filter(Boolean),
+          },
+          socket,
+          false
+        );
+        break;
+
+      case "message":
+        const message = wsObjReceived.data;
+        broadcastMessage(
+          {
+            type: "message",
+            data: {
+              message,
+              fromUser: connections.get(socket).userLogin,
+              image: connections.get(socket).image,
+            },
+          },
+          socket,
+          false
+        );
+        break;
+
+      case "avatar":
+        const image = wsObjReceived.data;
+        connections.get(socket).image = image;
+
+        broadcastMessage(
+          {
+            type: "avatar",
+            data: {
+              image,
+              fromUser: connections.get(socket).userLogin,
+            },
+          },
+          socket,
+          false
+        );
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  socket.on("close", () => {
+    console.log("Disconnect");
+    broadcastMessage(
+      {
+        type: "log-out",
+        data: [...connections.values()]
+          .map(
+            (item) =>
+              connections.get(socket).userLogin !== item.userLogin &&
+              item.userLogin
+          )
+          .filter(Boolean),
+      },
+      socket
+    );
+    connections.delete(socket);
+  });
+});
+
+function broadcastMessage(message, fromUser, excludeSelf = true) {
+  const socketData = connections.get(fromUser);
+
+  if (!socketData) return false;
+
+  // define user login of sender
+  message.fromUser = socketData.userLogin;
+
+  for (const connection of connections.keys()) {
+    if (connection === fromUser && excludeSelf) {
+      continue;
+    }
+
+    connection.send(JSON.stringify(message));
+  }
+}
+
+// if no 3000 will look for environment vrbl named port
+const PORT = 3000 || process.env.PORT;
+server.listen(PORT, () => {
+  console.log(`connection established on port: ${PORT}`);
 });
